c96f5e0415b599f21c0b0f283eca4d537f66a8cc
Flavio Augusto Rodrigues <flavius.rodrigues@gmail.com>
2024-03-29 18:22:15 -0300

    Fim project push

--- main.js
+++ main.js
@@ -15,4 +15,15 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function() {
         tocaSom(idAudio);
     }
+    tecla.onkeydown = function (event) {
+        
+        if(event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
